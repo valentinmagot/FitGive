fe246f80fb38625796452bca1e9477238841e312
Ellen Li <eli030@uottawa.ca>
2021-02-21 11:47:48 -0500

    tabs component

--- src/views/CompleteChallenge/Method.js
+++ src/views/CompleteChallenge/Method.js
@@ -8,6 +8,9 @@ import { Link } from "react-router-dom";
 // core components
 import styles from "assets/jss/material-dashboard-react/views/iconsStyle.js";
 import Button from "components/CustomButtons/Button.js";
+import GridContainer from "components/Grid/GridContainer";
+import GridItem from "components/Grid/GridItem";
+import CustomTabs from "components/CustomTabs/CustomTabs.js";
 
 const useStyles = makeStyles(styles);
 
@@ -17,6 +20,28 @@ export default function Method({ navigation }) {
 
     return (
         <>
+            <GridContainer>
+                <GridItem xs={12} sm={12} md={12}>
+                    <CustomTabs title="Methods:"
+                        headerColor="primary"
+                        tabs={[
+                            {
+                                tabName: "Manual",
+                                tabContent: (
+                                    <p>man</p>
+
+                                )
+                            },
+                            {
+                                tabName: "Automatic",
+                                tabContent: (
+                                    <p>auto</p>
+                                )
+                            },
+                        ]}
+                    />
+                </GridItem>
+            </GridContainer>
             Method
             <Button onClick={next}>Next</Button>
         </>
